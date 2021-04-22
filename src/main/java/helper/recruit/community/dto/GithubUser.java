aeb3ba8278e25626b9fb8ef6bdf3e700cb411530
Eric Yin <ericyin2016@gmail.com>
2021-04-22 16:16:51 +0800

    neat codes

--- src/main/java/helper/recruit/community/dto/GithubUser.java
+++ src/main/java/helper/recruit/community/dto/GithubUser.java
@@ -9,36 +9,4 @@ public class GithubUser {
     private String bio;
     private String avatarUrl;
 
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getBio() {
-//        return bio;
-//    }
-//
-//    public void setBio(String bio) {
-//        this.bio = bio;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "GithubUser{" +
-//                "name='" + name + '\'' +
-//                ", id=" + id +
-//                ", bio='" + bio + '\'' +
-//                '}';
-//    }
 }
