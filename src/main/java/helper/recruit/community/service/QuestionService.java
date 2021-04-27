dd579b098801d1744066220ada8bfbe65316284e
Eric Yin <ericyin2016@gmail.com>
2021-04-27 23:29:19 +0800

    add mybatis generator Version2

--- src/main/java/helper/recruit/community/service/QuestionService.java
+++ src/main/java/helper/recruit/community/service/QuestionService.java
@@ -5,7 +5,9 @@ import helper.recruit.community.dto.QuestionDTO;
 import helper.recruit.community.mapper.QuestionMapper;
 import helper.recruit.community.mapper.UserMapper;
 import helper.recruit.community.model.Question;
+import helper.recruit.community.model.QuestionExample;
 import helper.recruit.community.model.User;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,7 +29,7 @@ public class QuestionService {
         // page operation
         PaginationDTO paginationDTO = new PaginationDTO();
         Integer totalPage;
-        Integer totalCount = questionMapper.count();
+        Integer totalCount = (int) questionMapper.countByExample(new QuestionExample());
         // total count is the number of job list in the question database
         // page number
         // size is 5 : one page 5 rows
@@ -45,7 +47,8 @@ public class QuestionService {
         paginationDTO.setPagination(totalPage, page);
 
         Integer offset = size * (page - 1);
-        List<Question> questions = questionMapper.list(offset, size);
+        List<Question> questions = questionMapper.selectByExampleWithRowbounds(new QuestionExample(), new RowBounds(offset, size));
+
         List<QuestionDTO> questionDTOList = new ArrayList<>();
 
         for (Question question : questions) {
@@ -66,7 +69,10 @@ public class QuestionService {
 
         Integer totalPage;
 
-        Integer totalCount = questionMapper.countByUserId(userId);
+//        Integer totalCount = questionMapper.countByUserId(userId);
+        QuestionExample questionExample = new QuestionExample();
+        questionExample.createCriteria().andCreatorEqualTo(userId);
+        Integer totalCount = (int) questionMapper.countByExample(questionExample);
 
         // total count is the number of job list in the question database
         // page number
@@ -85,7 +91,10 @@ public class QuestionService {
         paginationDTO.setPagination(totalPage, page);
 
         Integer offset = size * (page - 1);
-        List<Question> questions = questionMapper.yourPostList(userId, offset, size);
+//        List<Question> questions = questionMapper.yourPostList(userId, offset, size);
+        QuestionExample example = new QuestionExample();
+        example.createCriteria().andCreatorEqualTo(userId);
+        List<Question> questions = questionMapper.selectByExampleWithRowbounds(example, new RowBounds(offset, size));
         List<QuestionDTO> questionDTOList = new ArrayList<>();
 
         for (Question question : questions) {
@@ -101,7 +110,7 @@ public class QuestionService {
     }
 
     public QuestionDTO getById(Integer id) {
-        Question question = questionMapper.getById(id);
+        Question question = questionMapper.selectByPrimaryKey(id);
         QuestionDTO questionDTO = new QuestionDTO();
         BeanUtils.copyProperties(question, questionDTO); // copy source attribute to target
         User user = userMapper.selectByPrimaryKey(question.getCreator());//get user
@@ -114,11 +123,22 @@ public class QuestionService {
             // create a new
             question.setGmtCreate(System.currentTimeMillis());
             question.setGmtModified(question.getGmtCreate());
-            questionMapper.create(question);
+            questionMapper.insert(question);
         }else{
             // update
-            question.setGmtModified(System.currentTimeMillis());
-            questionMapper.update(question);
+//            question.setGmtModified(System.currentTimeMillis());
+            Question updateQuestion = new Question();
+            updateQuestion.setGmtCreate(System.currentTimeMillis());
+            updateQuestion.setTitle(question.getTitle());
+            updateQuestion.setCompany(question.getCompany());
+            updateQuestion.setTag(question.getTag());
+            updateQuestion.setPlace(question.getPlace());
+            updateQuestion.setDescription(question.getDescription());
+            updateQuestion.setJoblink(question.getJoblink());
+
+            QuestionExample example = new QuestionExample();
+            example.createCriteria().andIdEqualTo(question.getId());
+            questionMapper.updateByExampleSelective(updateQuestion, example);
         }
     }
 }
